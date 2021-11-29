a5d0cefab260f95499256ac014ca600a5336ec0e
Chris <gichulroh@gmail.com>
2021-11-29 23:15:39 +0900

    Add modal component with register form

--- src/views/MovieInfo.js
+++ src/views/MovieInfo.js
@@ -1,7 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
-import { Row, Col, Button, Modal, Typography, Badge } from 'antd';
+import { 
+    Row, 
+    Col, 
+    Button, 
+    Modal, 
+    Typography, 
+    Badge, 
+    Form, 
+    Checkbox, 
+    InputNumber,
+    Input 
+} from 'antd';
 import { StarFilled } from '@ant-design/icons';
 const { Text, Title } = Typography;
 
@@ -36,6 +47,15 @@ const MovieInfo = (props) => {
         imageUrl,
     } = movie;
 
+    const layout = {
+        labelCol: {
+            span: 5,
+        },
+        wrapperCol: {
+            span: 19,
+        },
+    };
+    
     const showModal = () => {
         setIsModalVisible(true);
     };
@@ -47,6 +67,25 @@ const MovieInfo = (props) => {
     const handleCancel = () => {
         setIsModalVisible(false);
     }; 
+
+    const onFinish = (values) => {
+        console.log('Success:', values);
+    };
+
+    const validateMessages = {
+        required: '${label} is required!',
+        types: {
+            email: '${label} is not a valid email!',
+            number: '${label} is not a valid number!',
+        },
+        number: {
+            range: '${label} must be between ${min} and ${max}',
+        },
+    };
+
+    function onChange(checkedValues) {
+        console.log('checked = ', checkedValues);
+    }
     
     return (
         <>
@@ -105,9 +144,99 @@ const MovieInfo = (props) => {
                 onOk={handleOk} 
                 onCancel={handleCancel}
             >
-                <div>
-                    영화 수정하기
-                </div>
+                <FormContainer>
+                    <Form 
+                        {...layout} 
+                        name="nest-messages" 
+                        onFinish={onFinish}
+                        validateMessages={validateMessages}
+                    >
+                        <Form.Item
+                            name={['movies', 'title']}
+                            label="영화제목"
+                            rules={[
+                            {
+                                required: true,
+                            },
+                            ]}
+                        >
+                            <Input />
+                        </Form.Item>
+                        <Form.Item
+                            name={['movies', 'director']}
+                            label="감독"
+                            rules={[
+                            {
+                                required: true,
+                            },
+                            ]}
+                        >
+                            <Input />
+                        </Form.Item>
+                        <Form.Item
+                            name={['movies', 'year']}
+                            label="개봉연도"
+                            rules={[
+                            {
+                                type: 'number',
+                                min: 1900,
+                                max: 2022,
+                            },
+                            ]}
+                        >
+                            <InputNumber />
+                        </Form.Item>
+                        <Form.Item name={['movies', 'rating']} label="별점">
+                            <Input />
+                        </Form.Item>
+                        <Form.Item name={['movies', 'category']} label="장르">
+                            <Checkbox.Group style={{ width: '100%' }} onChange={onChange}>
+                                <Row>
+                                    <Col span={8}>
+                                        <Checkbox value="A">공포</Checkbox>
+                                    </Col>
+                                    <Col span={8}>
+                                        <Checkbox value="B">드라마</Checkbox>
+                                    </Col>
+                                    <Col span={8}>
+                                        <Checkbox value="C">SF</Checkbox>
+                                    </Col>
+                                    <Col span={8}>
+                                        <Checkbox value="D">스릴러</Checkbox>
+                                    </Col>
+                                    <Col span={8}>
+                                        <Checkbox value="E">액션</Checkbox>
+                                    </Col>
+                                    <Col span={8}>
+                                        <Checkbox value="F">다큐멘터리</Checkbox>
+                                    </Col>
+                                    <Col span={8}>
+                                        <Checkbox value="G">로맨스</Checkbox>
+                                    </Col>
+                                    <Col span={8}>
+                                        <Checkbox value="H">액션</Checkbox>
+                                    </Col>
+                                    <Col span={8}>
+                                        <Checkbox value="I">애니메이션</Checkbox>
+                                    </Col>
+                                </Row>
+                            </Checkbox.Group>
+                        </Form.Item>
+                        <Form.Item name={['movies', 'summary']} label="소개글">
+                            <Input.TextArea />
+                        </Form.Item>
+                        <Form.Item>
+                            <Button danger>
+                            삭제하기
+                            </Button>
+                        </Form.Item>
+                        <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
+                            <Button type="primary" htmlType="submit">
+                            등록하기
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                </FormContainer>
             </Modal>
         </>
     );
@@ -196,4 +325,7 @@ const MovieTitlebar = styled.div`
 
 const StyledMovieInfo = styled.div`
     padding: 80px 0 60px;
+`
+const FormContainer = styled.div`
+
 `
